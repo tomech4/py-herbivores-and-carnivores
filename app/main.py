7128eb15d8b85545d7d23f10bf24b8b1d508f8b3
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-01 23:21:17 +0200

    create Animal parent class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+class Animal:
+    alive = []
+    # include class instances, if health = 0, remove from the list
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.health = 100
+        self.hidden = False
+
